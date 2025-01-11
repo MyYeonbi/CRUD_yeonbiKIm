850e7f4941f8323a7bd4a9b8b2c258c1b36e067a
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-11 12:32:53 +0900

    ✨ Item 클래스 생성 _ 필드 설정, getter,setter, toString 메서드 생성

---
+++ src/Item.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Item {
+    //Item 클래스 작성
+    private int id;
+    private String name;
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name=" + name + '\'' +
+                '}';
+    }
+}
